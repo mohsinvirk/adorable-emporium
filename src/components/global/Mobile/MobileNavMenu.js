4de8c076b8ce078aac6a5529b58eac4c9182c7b1
Mohsin Latif <mohsinlatifvirk@gmail.com>
2018-07-10 21:04:13 +0500

    Changes in Mobile Nav menu

--- src/components/global/Mobile/MobileNavMenu.js
+++ src/components/global/Mobile/MobileNavMenu.js
@@ -1,20 +1,44 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const NavMenu = () => (
-  <nav id="mobile-nav" aria-hidden="true" className="pushy pushy-left">
-    <ul>
-      <li className="pushy-link">
-        <Link to="/products">Products</Link>
-      </li>
-      <li className="pushy-link">
-        <Link to="/styles">Styles</Link>
-      </li>
-      <li className="pushy-link">
-        <Link to="/myAccount">My Account</Link>
-      </li>
-    </ul>
-  </nav>
-);
+const NavMenu = () => {
+  const token = localStorage.getItem('customerToken');
+  if (!token) {
+    return (
+      <nav id="mobile-nav" aria-hidden="true" className="pushy pushy-left">
+        <ul>
+          <li className="pushy-link">
+            <Link to="/products">Products</Link>
+          </li>
+          <li className="pushy-link">
+            <Link to="/styles">Styles</Link>
+          </li>
+          <li className="pushy-link">
+            <Link to="/login">Login</Link>
+          </li>
+          <li className="pushy-link">
+            <Link to="/resgister">SignUp</Link>
+          </li>
+        </ul>
+      </nav>
+    );
+  } else {
+    return (
+      <nav id="mobile-nav" aria-hidden="true" className="pushy pushy-left">
+        <ul>
+          <li className="pushy-link">
+            <Link to="/products">Products</Link>
+          </li>
+          <li className="pushy-link">
+            <Link to="/styles">Styles</Link>
+          </li>
+          <li className="pushy-link">
+            <Link to="/myAccount">My Account</Link>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+};
 
 export default NavMenu;
